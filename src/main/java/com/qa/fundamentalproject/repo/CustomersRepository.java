89585c366488b7ac25e3cafc05a90634f03e98f5
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-27 15:07:16 +0100

    added the repository for the CustomersRepository

---
+++ src/main/java/com/qa/fundamentalproject/repo/CustomersRepository.java
@@ -0,0 +1,8 @@
+package com.qa.fundamentalproject.repo;
+import com.qa.fundamentalproject.domain.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomersRepository extends JpaRepository<Customers, Long> {
+}
