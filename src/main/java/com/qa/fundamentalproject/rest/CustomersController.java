cc9748fce61201340d0756cc58b52219725e8013
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-27 16:29:24 +0100

    added controller

---
+++ src/main/java/com/qa/fundamentalproject/rest/CustomersController.java
@@ -0,0 +1,45 @@
+package com.qa.fundamentalproject.rest;
+import com.qa.fundamentalproject.domain.Customers;
+import com.qa.fundamentalproject.service.CustomersService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+
+public class CustomersController {
+    private final CustomersService customersService;
+
+    @Autowired
+    public CustomersController(CustomersService customersService) {
+        this.customersService = customersService;
+    }
+
+    @GetMapping("/")
+    public List<Customers> getAllCustomers(){
+        return this.customersService.readAllCustomers();
+    }
+
+    @PostMapping("/createCustomer")
+    public Customers createNote(@RequestBody Customers customer){
+        return this.customersService.createCustomers(customer);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteCustomer(@PathVariable Long id){
+        return this.customersService.deleteCustomersById(id);
+    }
+
+    @GetMapping("/getCustomerById/{id}")
+    public Customers getCustomersById(@PathVariable Long id){
+        return this.customersService.findCustomerById(id);
+    }
+
+    @PutMapping("/updateNote/{id}")
+    public Customers updateCustomers(@PathVariable Long id, @RequestBody Customers customer){
+        return this.customersService.updateCustomers(id, customer);
+    }
+
+}
