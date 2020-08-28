edff2c681576dde6e4dfc47e39835e94f1a3c796
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-28 11:35:13 +0100

    added the repository for BookingRepository

---
+++ src/main/java/com/qa/fundamentalproject/repo/BookingRepository.java
@@ -0,0 +1,8 @@
+package com.qa.fundamentalproject.repo;
+import com.qa.fundamentalproject.domain.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
