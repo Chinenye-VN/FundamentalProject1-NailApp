2d833b87b73d2400e52ffef6f196cda986ab4f0b
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-28 11:48:13 +0100

    added crud functionality to Bookings

---
+++ src/main/java/com/qa/fundamentalproject/service/BookingService.java
@@ -0,0 +1,47 @@
+package com.qa.fundamentalproject.service;
+import com.qa.fundamentalproject.domain.Booking;
+import com.qa.fundamentalproject.repo.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.qa.fundamentalproject.exceptions.BookingNotFoundException;
+
+import java.awt.print.Book;
+import java.util.List;
+
+@Service
+public class BookingService {
+    private final BookingRepository repo;
+
+    @Autowired
+    public BookingService(BookingRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Booking> readAllBooking(){
+        return this.repo.findAll();
+    }
+
+    public Booking createBooking(Booking booking){
+
+        return this.repo.save(booking);
+    }
+
+    public Booking findBookingById(Long id){
+        return this.repo.findById(id).orElseThrow(BookingNotFoundException::new);
+    }
+
+    public Booking updateBooking(Long id, Booking booking){
+        Booking update = findBookingById(id);
+        update.setDateBooked(booking.getDateBooked());
+        update.setTotal(booking.getTotal());
+        return this.repo.save(update);
+    }
+
+    public Boolean deleteBookingById(Long id){
+        if(!this.repo.existsById(id)){
+            throw new BookingNotFoundException(); //if the id doesnt exist return the exception "this booking isn't found"
+        }
+        this.repo.deleteById(id);
+        return this.repo.existsById(id);
+    }
+}
