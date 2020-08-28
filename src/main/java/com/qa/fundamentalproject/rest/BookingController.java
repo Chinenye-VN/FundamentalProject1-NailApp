0919e449cae008e7d7b7297f9f8caa7ff30502af
Chinenye-VN <veronica.nwandu@gmail.com>
2020-08-28 13:31:06 +0100

    added crud functionality for the controller of Bookings

---
+++ src/main/java/com/qa/fundamentalproject/rest/BookingController.java
@@ -0,0 +1,43 @@
+package com.qa.fundamentalproject.rest;
+import com.qa.fundamentalproject.domain.Booking;
+import com.qa.fundamentalproject.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class BookingController {
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("/getAllBooking")
+    public List<Booking> getAllBooking(){
+        return this.bookingService.readAllBooking();
+    }
+
+    @PostMapping("/createBooking")
+    public Booking createBooking(@RequestBody Booking booking){
+        return this.bookingService.createBooking(booking);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean deleteBooking(@PathVariable Long id){
+        return this.bookingService.deleteBookingById(id);
+    }
+
+    @GetMapping("/getBookingById/{id}")
+    public Booking getBookingById(@PathVariable Long id){
+        return this.bookingService.findBookingById(id);
+    }
+
+    @PutMapping("/updateBooking/{id}")
+    public Booking updateBooking(@PathVariable Long id, @RequestBody Booking booking){
+        return this.bookingService.updateBooking(id, booking);
+    }
+
+}
