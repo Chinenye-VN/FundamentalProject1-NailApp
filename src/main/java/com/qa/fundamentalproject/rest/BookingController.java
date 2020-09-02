2f10f52e638fb5f330b04d7a540d3e05b3eede21
Chinenye-VN <veronica.nwandu@gmail.com>
2020-09-02 23:31:56 +0100

    updated BookingService to return DTOs

--- src/main/java/com/qa/fundamentalproject/rest/BookingController.java
+++ src/main/java/com/qa/fundamentalproject/rest/BookingController.java
@@ -1,8 +1,12 @@
 package com.qa.fundamentalproject.rest;
+
 import com.qa.fundamentalproject.domain.Booking;
+import com.qa.fundamentalproject.dto.BookingDTO;
 import com.qa.fundamentalproject.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 import java.util.List;
 
 @RestController
@@ -15,29 +19,31 @@ public class BookingController {
     }
 
     @GetMapping("/getAllBooking")
-    public List<Booking> getAllBooking(){
-        return this.bookingService.readAllBooking();
+    public ResponseEntity<List<BookingDTO>>getAllBooking(){
+        return ResponseEntity.ok(this.bookingService.readAllBooking());
     }
 
     @PostMapping("/createBooking")
-    public Booking createBooking(@RequestBody Booking booking){
-        return this.bookingService.createBooking(booking);
+    public ResponseEntity<BookingDTO> createBooking(@RequestBody Booking booking){
+        return new ResponseEntity<BookingDTO>(this.bookingService.createBooking(booking), HttpStatus.CREATED);
 
     }
 
     @DeleteMapping("/deleteBooking/{id}")
-    public Boolean deleteBooking(@PathVariable Long id){
-        return this.bookingService.deleteBookingById(id);
+    public ResponseEntity<?> deleteBooking(@PathVariable Long id){
+        return this.bookingService.deleteBookingById(id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
     }
 
     @GetMapping("/getBookingById/{id}")
-    public Booking getBookingById(@PathVariable Long id){
-        return this.bookingService.findBookingById(id);
+    public ResponseEntity<BookingDTO> getBookingById(@PathVariable Long id){
+        return ResponseEntity.ok(this.bookingService.findBookingById(id));
     }
 
     @PutMapping("/updateBooking/{id}")
-    public Booking updateBooking(@PathVariable Long id, @RequestBody Booking booking){
-        return this.bookingService.updateBooking(id, booking);
+    public ResponseEntity<BookingDTO> updateBooking(@PathVariable Long id, @RequestBody Booking booking){
+        return ResponseEntity.ok(this.bookingService.updateBooking(id, booking));
     }
 
 }
--- src/main/java/com/qa/fundamentalproject/service/BookingService.java
+++ src/main/java/com/qa/fundamentalproject/service/BookingService.java
@@ -1,45 +1,54 @@
 package com.qa.fundamentalproject.service;
+
 import com.qa.fundamentalproject.domain.Booking;
+import com.qa.fundamentalproject.dto.BookingDTO;
 import com.qa.fundamentalproject.repo.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.qa.fundamentalproject.exceptions.BookingNotFoundException;
+import com.qa.fundamentalproject.exceptions.CustomersNotFoundException;
+import org.modelmapper.ModelMapper;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookingService {
     private final BookingRepository repo;
+    private  final ModelMapper mapper;
 
     @Autowired
-    public BookingService(BookingRepository repo) {
+    public BookingService(BookingRepository repo, ModelMapper mapper) {
         this.repo = repo;
+        this.mapper = mapper;
     }
 
-    public List<Booking> readAllBooking(){
-        return this.repo.findAll();
+    private BookingDTO mapToDTO(Booking booking) {
+        return this.mapper.map(booking,BookingDTO.class);
     }
 
-    public Booking createBooking(Booking booking){
-
-        return this.repo.save(booking);
+    public List<BookingDTO> readAllBooking(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public Booking findBookingById(Long id){
-        return this.repo.findById(id).orElseThrow(BookingNotFoundException::new);
+    public BookingDTO createBooking(Booking booking){
+        return this.mapToDTO(this.repo.save(booking));
     }
 
-    public Booking updateBooking(Long id, Booking booking){
-        Booking update = findBookingById(id);
+    public BookingDTO findBookingById(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(BookingNotFoundException::new));
+    }
+
+    public BookingDTO updateBooking(Long id, Booking booking){
+        Booking update = this.repo.findById(id).orElseThrow(BookingNotFoundException::new);
         update.setTreatment(booking.getTreatment());
         update.setDateBooked(booking.getDateBooked());
         update.setTotalPrice(booking.getTotalPrice());
-
-        return this.repo.save(update);
+        return this.mapToDTO(this.repo.save(update));
     }
 
     public Boolean deleteBookingById(Long id){
         if(!this.repo.existsById(id)){
-            throw new BookingNotFoundException(); //if the id doesnt exist return the exception "this booking isn't found"
+            throw new CustomersNotFoundException(); //if the id doesnt exist return the exception "this customer isn't found"
         }
         this.repo.deleteById(id);
         return this.repo.existsById(id);
