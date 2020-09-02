536b9f82fe2d39521e19554f5b5a800fc9e0bb9c
Chinenye-VN <veronica.nwandu@gmail.com>
2020-09-02 23:59:47 +0100

    updated CustomerService to return DTOs

--- src/main/java/com/qa/fundamentalproject/rest/CustomersController.java
+++ src/main/java/com/qa/fundamentalproject/rest/CustomersController.java
@@ -1,7 +1,10 @@
 package com.qa.fundamentalproject.rest;
 import com.qa.fundamentalproject.domain.Customers;
+import com.qa.fundamentalproject.dto.CustomersDTO;
 import com.qa.fundamentalproject.service.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -12,32 +15,36 @@ public class CustomersController {
 
     @Autowired
     public CustomersController(CustomersService customersService) {
+
         this.customersService = customersService;
     }
 
     @GetMapping("/getAllCustomers")
-    public List<Customers> getAllCustomers(){
-        return this.customersService.readAllCustomers();
+    public ResponseEntity<List<CustomersDTO>> getAllCustomers(){
+
+        return ResponseEntity.ok(this.customersService.readAllCustomers());
     }
 
     @PostMapping("/createCustomer")
-    public Customers createNote(@RequestBody Customers customer){
-        return this.customersService.createCustomers(customer);
+    public ResponseEntity<CustomersDTO> createCustomers(@RequestBody Customers customer){
+        return new ResponseEntity<CustomersDTO>(this.customersService.createCustomers(customer), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/deleteCustomers/{id}")
-    public Boolean deleteCustomer(@PathVariable Long id){
-        return this.customersService.deleteCustomersById(id);
+    public ResponseEntity<?> deleteCustomer(@PathVariable Long id){
+        return this.customersService.deleteCustomersById(id)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.noContent().build();
     }
 
     @GetMapping("/getCustomerById/{id}")
-    public Customers getCustomersById(@PathVariable Long id){
-        return this.customersService.findCustomerById(id);
+    public ResponseEntity<CustomersDTO> getCustomersById(@PathVariable Long id){
+        return ResponseEntity.ok(this.customersService.findCustomerById(id));
     }
 
     @PutMapping("/updateCustomer/{id}")
-    public Customers updateCustomers(@PathVariable Long id, @RequestBody Customers customer){
-        return this.customersService.updateCustomers(id, customer);
+    public ResponseEntity<CustomersDTO> updateCustomers(@PathVariable Long id, @RequestBody Customers customer){
+        return ResponseEntity.ok(this.customersService.updateCustomers(id, customer));
     }
 
 }
--- src/main/java/com/qa/fundamentalproject/service/CustomersService.java
+++ src/main/java/com/qa/fundamentalproject/service/CustomersService.java
@@ -1,41 +1,49 @@
 package com.qa.fundamentalproject.service;
 
 import com.qa.fundamentalproject.domain.Customers;
+import com.qa.fundamentalproject.dto.CustomersDTO;
 import com.qa.fundamentalproject.repo.CustomersRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.qa.fundamentalproject.exceptions.CustomersNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomersService {
     private final CustomersRepository repo;
+    private final ModelMapper mapper;
 
     @Autowired
-    public CustomersService(CustomersRepository repo) {
+    public CustomersService(CustomersRepository repo, ModelMapper mapper) {
         this.repo = repo;
+        this.mapper = mapper;
+    }
+    private CustomersDTO mapToDTO(Customers customers) {
+        return this.mapper.map(customers,CustomersDTO.class);
     }
 
-    public List<Customers> readAllCustomers(){
-        return this.repo.findAll();
+    public List<CustomersDTO> readAllCustomers(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 
-    public Customers createCustomers(Customers customers){
+    public CustomersDTO createCustomers(Customers customers){
 
-        return this.repo.save(customers);
+        return this.mapToDTO(this.repo.save(customers));
     }
 
-    public Customers findCustomerById(Long id){
-        return this.repo.findById(id).orElseThrow(CustomersNotFoundException::new);
+    public CustomersDTO findCustomerById(Long id){
+        return this.mapToDTO(this.repo.findById(id).orElseThrow(CustomersNotFoundException::new));
     }
 
-    public Customers updateCustomers(Long id, Customers customer){
-        Customers update = findCustomerById(id);
+    public CustomersDTO updateCustomers(Long id, Customers customer){
+        Customers update = this.repo.findById(id).orElseThrow(CustomersNotFoundException::new);
         update.setName(customer.getName());
         update.setEmail(customer.getEmail());
         update.setPassword(customer.getPassword());
         update.setPhoneNumber(customer.getPhoneNumber());
-        return this.repo.save(update);
+        return this.mapToDTO(this.repo.save(update));
     }
 
     public Boolean deleteCustomersById(Long id){
