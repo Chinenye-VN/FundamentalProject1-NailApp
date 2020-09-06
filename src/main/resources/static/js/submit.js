5fb7ae38a3da524fc61b768cb94a87fc98d7e28b
Chinenye-VN <veronica.nwandu@gmail.com>
2020-09-06 20:38:31 +0100

    added final edits to the homepage

--- src/main/resources/static/js/submit.js
+++ src/main/resources/static/js/submit.js
@@ -90,3 +90,29 @@ function deleteCustomer(id) {
     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     req.send();
 }
+
+function updateCustomer() {
+    let elements = document.getElementById("updateForm").elements;
+    let obj = {};
+    for (let i = 0; i < elements.length - 1; i++) {
+        let item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+    const url = "http://localhost:8080/updateCustomer/" +obj.bookingid;
+    const req = new XMLHttpRequest();
+    req.open("PUT", url);
+    req.onload = () => {
+        if (req.status === 200 && req.readyState === 4) {
+            console.log("Server Responded with: " + req.responseText);
+
+        } else {
+            console.log("Customer doesn't exist...");
+        }
+    };
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify({
+        name: obj.name, email: obj.email, phoneNumber: obj.phoneNumber, booking: {
+            id: Number(obj.bookingid)
+        }
+    }));
+}
