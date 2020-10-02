057cf5940831b0745c5d6b99ab7ef74a436c5bc7
Chinenye Nwandu <67292767+Chinenye-VN@users.noreply.github.com>
2020-10-02 14:44:33 +0100

    Create AccountManagement.java

---
+++ src/main/java/com/qa/fundamentalproject/domain/AccountManagement.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringApplication;
+
+@SpringBootApplication
+public class AccountManagement {
+    public static void main(String[] args) {
+       SpringApplication.run(AccountManagement.class, args);
+    }
+}
